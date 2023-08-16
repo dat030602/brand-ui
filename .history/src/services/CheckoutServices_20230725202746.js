60ae72ebfdec1a1b3456a4a2155ad514c34b0d18
Nguyễn Văn Đạt <74915138+datmap233@users.noreply.github.com>
2023-08-16 20:02:47 +0700

    Delete .history/src directory

--- .history/src/services/CheckoutServices_20230725202746.js
+++
@@ -1,9 +0,0 @@
-import * as httpRequest from "~/utils/httpRequest";
-export const GetCartItem = async (username = "") => {
-  try {
-    const res = await httpRequest.Get(`/user/cartitem?id_user=${username}`);
-    return res;
-  } catch (error) {
-    console.log("error");
-  }
-};
