655223f3e4c69d67da16d20a286b8c5b8ecd62cf
Yooni <20127251@student.hcmus.edu.vn>
2023-09-04 15:10:46 +0700

    add feature chatbox and related files

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -210,7 +210,18 @@ function Header({ children }) {
                 )}
                 {isLogin && (
                   <>
-                    <div className="col ml-2">
+                   <div className="col ml-10 d-flex">
+                      <a href="https://m.me/lethungan.190702" className={`${styles['action-icon']}`}>
+                        <Image
+                          src={`${window.location.href.split('/').length - 1 >= 4 ? '../' : ''}${
+                            window.location.href.split('/').length - 1 >= 3 ? '../' : ''
+                          }${window.location.href.split('/').length - 1 >= 2 ? '.' : ''}./assets/svg/chatbox.svg`}
+                          alt="/"
+                        />
+                        <span>Chatbox</span>
+                      </a>
+                    </div>
+                    <div className="col ml-10 d-flex">
                       <a href="/personal/edit" className={`${styles['action-icon']}`}>
                         <Image
                           src={`${window.location.href.split('/').length - 1 >= 4 ? '../' : ''}${
@@ -221,7 +232,7 @@ function Header({ children }) {
                         <span>Profile</span>
                       </a>
                     </div>
-                    <div className="col ml-2">
+                    <div className="col ml-10 d-flex">
                       <a href="/favorite" className={`${styles['action-icon']}`}>
                         <Image
                           src={`${window.location.href.split('/').length - 1 >= 4 ? '../' : ''}${
@@ -232,7 +243,7 @@ function Header({ children }) {
                         <span>Favorite</span>
                       </a>
                     </div>
-                    <div className="col ml-2">
+                    <div className="col ml-10 d-flex">
                       <a href="/my-cart" className={`${styles['action-icon']}`}>
                         <Image
                           src={`${window.location.href.split('/').length - 1 >= 4 ? '../' : ''}${
