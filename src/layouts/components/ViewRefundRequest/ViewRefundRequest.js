5afe244db046d42fed5726888162a7acae6833a5
bingsuxoaii <ngtphuongtrang67@gmail.com>
2023-09-08 19:55:55 +0700

    fix view refund's payment

--- src/layouts/components/ViewRefundRequest/ViewRefundRequest.js
+++ src/layouts/components/ViewRefundRequest/ViewRefundRequest.js
@@ -150,7 +150,7 @@ function ViewRefundRequest({ children }) {
                             <div className="d-flex">
                               <span className="text-gray">Payment:</span>
                               <span className="text-primary pl-1 text-bold-normal">
-                                {orderRefundRequest[0].LOAI_THANHTOAN}
+                                {orderRefundRequest[0].LOAI_THANH_TOAN}
                               </span>
                             </div>
                             <div className="d-flex">
